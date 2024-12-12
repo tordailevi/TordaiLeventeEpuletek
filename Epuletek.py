bc0274a13b02fa54ef83d63ac3f6e2f0fe8ab5d6
Torderius <tordailevi@gmail.com>
2024-12-12 10:33:17 +0100

    majdnem kész

---
+++ Epuletek.py
@@ -0,0 +1,18 @@
+# epuletek.py
+class Epulet:
+    def __init__(self, nev, varos, orszag, magassag, emeletek_szama, epites_eve):
+        self.nev = nev
+        self.varos = varos
+        self.orszag = orszag
+        self.magassag = float(magassag)
+        self.emeletek_szama = int(emeletek_szama)
+        self.epites_eve = int(epites_eve)
+
+    '''def __str__(self):
+        return f"{self.nev} ({self.varos}, {self.orszag}) - {self.magassag} m, {self.emeletek_szama} emelet, {self.epites_eve}"'''
+    
+
+
+
+
+                                                                                                                                         
